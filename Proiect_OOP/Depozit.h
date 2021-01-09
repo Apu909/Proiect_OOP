b133c8dcfc68446908c0f34c5699bef9a80cc4fb
manolaqe <manolachemihaiandrei@protonmail.ch>
2021-01-09 20:20:06 +0200

    Produs modified

--- Proiect_OOP/Depozit.h
+++ Proiect_OOP/Depozit.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <iostream>
+#include <list>
+#include "Produs.h"
 class Depozit
 {
+	list <Produs> listProduseinStoc;
+	list <Produs>::iterator it;
+public:
+	Depozit();
+	void stergeProdusdinStoc();
+	void adaugaProdusinStoc();
+	void editeazaProdusdinStoc();
+	void veziProduseinStoc();
 };
 
